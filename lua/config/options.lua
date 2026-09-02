693ecdc5e223425f711ed50c2c676f22bacdb360
afschy <farhan.shahriar.2015@gmail.com>
2026-09-02 17:59:46 +0600

    enabled copying to system clipboard over ssh

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,6 +2,16 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 -- lua/config/options.lua
+-- lua/config/options.lua
+local is_remote = vim.env.NVIM_OSC52
+if is_remote then
+    local osc52 = require("vim.ui.clipboard.osc52")
+    vim.g.clipboard = {
+        name = "OSC 52",
+        copy = { ["+"] = osc52.copy("+"), ["*"] = osc52.copy("+") },
+        paste = { ["+"] = function() return {} end, ["*"] = function() return {} end },
+    }
+end
 vim.opt.relativenumber = true -- makes 5j / 12k natural
 vim.opt.scrolloff = 8
 -- vim.opt.colorcolumn = "100"
